e54e5de5592b8c6005891f30a115abaff7979e70
Ilias Abrak <55558747+Choulias@users.noreply.github.com>
2026-04-30 11:29:15 +0200

    feat: add terraform folder/files

---
+++ terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "location" {
+  description = "Région Azure"
+  type        = string
+  default     = "swedencentral"
+}
+
+variable "vm_size" {
+  description = "Taille de la VM Azure"
+  type        = string
+  default     = "Standard_D4s_v4"
+}
+
+variable "ssh_public_key" {
+  description = "Clé SSH publique"
+  type        = string
+}
+
+variable "db_name" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_user" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_root_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "jwt_secret" {
+  type      = string
+  sensitive = true
+}
